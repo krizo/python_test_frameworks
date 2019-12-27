2f12341a9084e083d88cdecd59afb5f54221abb3
Krzysztof Bober <kbober@zendesk.com>
2019-12-27 23:12:59 +0100

    Adding tests for popular shows

--- tests/test_tmdbwraper.py
+++ tests/test_tmdbwraper.py
@@ -20,3 +20,15 @@ def test_tv_info(tv_keys):
     assert isinstance(response, dict)
     assert response['id'] == 1396
     assert set(tv_keys).issubset(response.keys())
+
+
+@my_vcr.use_cassette('tests/vcr_cassettes/tv-popular.yml')
+def test_tv_popular(tv_keys):
+    """Tests an API call to get a popular tv shows"""
+
+    response = TV.popular()
+
+    assert isinstance(response, dict)
+    assert isinstance(response['results'], list)
+    assert isinstance(response['results'][0], dict)
+    assert set(tv_keys).issubset(response['results'][0].keys())
